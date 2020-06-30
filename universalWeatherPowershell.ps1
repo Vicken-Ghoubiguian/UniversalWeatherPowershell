5421b6e721094d5c60966a560c516f9464f07761
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-06-30 21:05:43 +0200

    Init commit

---
+++ universalWeatherPowershell.ps1
@@ -0,0 +1,35 @@
+﻿class universalWeatherPowershell 
+{
+
+       [int]$longitude
+       [int]$latitude
+
+       [string]$mainDescription
+       [string]$precisedDescription
+
+       [float]$temperature
+       [float]$feelingLikeTemperature
+       [float]$minTemperature
+       [float]$maxTemperature
+
+       [float]$pressure
+       [float]$humidity
+
+       [float]$windSpeed
+       [int]$windDeg
+       [float]$windGust
+
+       [long]$sunrise
+       [long]$sunset
+
+       [string]$countryCode
+       [string]$cityName
+
+       [int]$uvIndex
+       [int]$uvRisk
+
+       universalWeatherPowershell() 
+       {
+
+       }
+}
