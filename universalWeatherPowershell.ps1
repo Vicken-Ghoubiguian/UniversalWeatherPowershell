ea7ca886afe00f503a7ee0d202731c37bfbc4cf1
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-01 16:00:06 +0200

    Deployment of hashtags for futures comments

--- universalWeatherPowershell.ps1
+++ universalWeatherPowershell.ps1
@@ -1,37 +1,48 @@
 ﻿class universalWeatherPowershell 
 {
 
+       #
        [int]$longitude
        [int]$latitude
 
+       #
        [string]$mainDescription
        [string]$precisedDescription
 
+       #
        [float]$temperature
        [float]$feelingLikeTemperature
        [float]$minTemperature
        [float]$maxTemperature
 
+       #
        [float]$pressure
        [float]$humidity
 
+       #
        [float]$windSpeed
        [int]$windDeg
        [float]$windGust
 
+       #
        [long]$sunrise
        [long]$sunset
 
+       #
        [string]$countryCode
        [string]$cityName
 
+       #
        [int]$uvIndex
        [int]$uvRisk
 
+       #
        universalWeatherPowershell([string] $city, [string] $apiKey) 
        {
+            #
             $weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&appid=" + $apiKey
 
+            #
             try {
 
                $weatherRequest = Invoke-WebRequest -Uri $weatherURL -Method Get
