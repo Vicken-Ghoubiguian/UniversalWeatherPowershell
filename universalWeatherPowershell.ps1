043b99c276501925a3b0c3dc9b201474b7633b3e
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-02 13:46:22 +0200

    Writing comments

--- universalWeatherPowershell.ps1
+++ universalWeatherPowershell.ps1
@@ -1,45 +1,45 @@
 ﻿class universalWeatherPowershell 
 {
 
-       #
+       # Attributes for respectively longitude and latitude
        [int]$longitude
        [int]$latitude
 
-       #
+       # Attributes for respectively main description and description with more precisions
        [string]$mainDescription
        [string]$precisedDescription
 
-       #
+       # Attributes for all datas concerning temperature
        [float]$temperature
        [float]$feelingLikeTemperature
        [float]$minTemperature
        [float]$maxTemperature
 
-       #
+       # Attributes for respectively pressure and humidity
        [float]$pressure
        [float]$humidity
 
-       #
+       # Attributes for all datas concerning wind
        [float]$windSpeed
        [int]$windDeg
        [float]$windGust
 
-       #
+       # Attributes for respectively sunrise and sunset times as timestamp
        [long]$sunrise
        [long]$sunset
 
-       #
+       # Attributes for all datas concerning wished location
        [string]$countryCode
        [string]$cityName
 
-       #
+       # Attributes for all datas concerning UV
        [int]$uvIndex
        [int]$uvRisk
 
-       #
+       # universalWeatherPowershell class constructor
        universalWeatherPowershell([string] $city, [string] $apiKey) 
        {
-            #
+            # Create an HTTP request to take the current weather, execute it and assign its value to the $weatherURL variable
             $weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&appid=" + $apiKey
 
             #
