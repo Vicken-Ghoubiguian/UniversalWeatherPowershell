ba449d65f246f4e7e89da537e9ebfaa38299645a
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-14 15:25:53 +0200

    Creating the 'samples' dir and put the first sample inside

---
+++ samples/sample_1.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module $PSScriptRoot\..\universalWeatherPowershell.ps1
+
+$a = [universalWeatherPowershell]::new("Paris", [temperatureMeasurementUnit]::Kelvin, [dateAndTimeFormat]::TimestampFormat, "YOUR API KEY")
