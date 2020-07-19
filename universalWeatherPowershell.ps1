473406571e5aa16ae5bf73fde2e0b80476a4594e
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-19 19:32:03 +0200

    Simplifications

--- universalWeatherPowershell.ps1
+++ universalWeatherPowershell.ps1
@@ -1,14 +1,6 @@
 ﻿Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell\usedModules\ultraviolet.psm1
 Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell\usedModules\temperature.psm1
-
-# Definition of the dateAndTimeFormat Powershell enum to get the wished date and time format
-Enum dateAndTimeFormat {
-
-    TimestampFormat = 0
-    DMYHMSFormat = 1
-    YMDHMSFormat = 2
-    MDYHMSFormat = 3
-}
+Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell\usedModules\dateandtime.psm1
 
 # Definition of the languageCode Powershell enum to get the wished language
 Enum languageCode {
@@ -46,9 +38,6 @@ class universalWeatherPowershell
        [int]$windDeg
        [float]$windGust
 
-       # Attribute for date and time format
-       [dateAndTimeFormat]$dateAndTimeFormatId
-
        # Attributes for respectively sunrise and sunset dates and times
        [long]$sunrise
        [long]$sunset
@@ -64,86 +53,8 @@ class universalWeatherPowershell
        # Attributes for all datas concerning UV
        [ultraviolet]$uv
 
-       # universalWeatherPowershell class minimal constructor
-       universalWeatherPowershell([string] $city, [string] $apiKey)
-       {
-
-            ###########################################################################
-            # Recovering all the weather datas values and extracting them all...      #
-            ###########################################################################
-
-            # Create an HTTP request to take the current weather, execute it and assign its value to the $weatherURL variable
-            $weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&appid=" + $apiKey
-
-            # Bloc we wish execute to get all informations about general weather datas (WEATHER BLOC)
-            try {
-
-               $weatherRequest = Invoke-WebRequest -Uri $weatherURL -Method Get
-
-               $weatherRequestsContent = $weatherRequest.Content
-
-               $weatherRequestsJSONContent = @"
-               
-$weatherRequestsContent
-
-"@
-
-               $weatherRequestsResults = ConvertFrom-Json -InputObject $weatherRequestsJSONContent
-
-               # Bloc we wish execute to get all informations about uv index (UV BLOC)
-               try {
-
-                   # Allocating the values of longitude and latitude in the attributes longitude and latitude respectively
-                   $this.longitude = [convert]::ToDouble($weatherRequestsResults.coord.lon)
-                   $this.latitude = [convert]::ToDouble($weatherRequestsResults.coord.lat)
-
-                    ###########################################################################
-                    # Extracting the UV index value and determine the UV risk...              #
-                    ###########################################################################
-
-                    # 
-                    $uviURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + $apiKey + "&lat=" + $this.latitude + "&lon=" + $this.longitude
-
-                    $uviRequest = Invoke-WebRequest -Uri $uviURL -Method Get
-
-                    $uviRequestsJSONContent = $uviRequest.Content
-
-                    $uviRequestsHashTable = ConvertFrom-Json $uviRequestsJSONContent
-
-                    # Allocating the value of the UV index in the attribute uvIndex
-                    $this.uv = [ultraviolet]::new([System.Math]::Floor([convert]::ToDouble($uviRequestsHashTable.value)))
-
-                    [System.Windows.MessageBox]::Show("Congradulations, you can now play with weather: " + $weatherRequestsContent, "Success...", "Ok", "Info")
-
-               # Bloc to execute if an System.Net.WebException is encountered (UV BLOC)
-               } catch [System.Net.WebException] {
-
-                  $errorType = $_.Exception.GetType().Name
-
-                  $errorMessage = $_.Exception.Message
-
-                  $errorStackTrace = $_.Exception.StackTrace
-
-                  [System.Windows.MessageBox]::Show("Sorry but an error occured when executing request. " + $errorMessage, "Error occured", "Ok", "Error")
-
-               }
-
-            # Bloc to execute if an System.Net.WebException is encountered (WEATHER BLOC)
-            } catch [System.Net.WebException] {
-
-               $errorType = $_.Exception.GetType().Name
-
-               $errorMessage = $_.Exception.Message
-
-               $errorStackTrace = $_.Exception.StackTrace
-
-               [System.Windows.MessageBox]::Show("Sorry but an error occured when executing request. " + $errorMessage, "Error occured", "Ok", "Error")
-
-            }
-       }
-
        # universalWeatherPowershell class constructor with all needed parameters
-       universalWeatherPowershell([string] $city, [string] $apiKey, [languageCode] $choosenLanguageCode, [dateAndTimeFormat] $choosenDateAndTimeFormat) 
+       universalWeatherPowershell([string] $city, [string] $apiKey, [languageCode] $choosenLanguageCode) 
        {
 
             ###########################################################################
@@ -175,25 +86,15 @@ $weatherRequestsContent
                     $this.longitude = [convert]::ToDouble($weatherRequestsResults.coord.lon)
                     $this.latitude = [convert]::ToDouble($weatherRequestsResults.coord.lat)
 
-                    # Allocating the value of the choosen date and time format to the dateAndTimeFormatId class attribute
-                    $this.dateAndTimeFormatId = $choosenDateAndTimeFormat
-
-                    If($this.dateAndTimeFormatId -eq [dateAndTimeFormat]::TimestampFormat) {
-
-                        # Allocating the value of sunrise time and sunset time to the respectives class attributes
-                        $this.sunrise = $weatherRequestsResults.sys.sunrise
-                        $this.sunset = $weatherRequestsResults.sys.sunset
-
-                    }
+                    # Allocating the value of sunrise time and sunset time to the respectives class attributes
+                    $this.sunrise = $weatherRequestsResults.sys.sunrise
+                    $this.sunset = $weatherRequestsResults.sys.sunset
 
                     # Allocating all temperatures to their respectives objects
                     $this.temperature = [temperature]::new([convert]::ToDouble($weatherRequestsResults.main.temp))
                     $this.feelingLikeTemperature = [temperature]::new([convert]::ToDouble($weatherRequestsResults.main.feels_like))
                     $this.minTemperature = [temperature]::new([convert]::ToDouble($weatherRequestsResults.main.temp_min))
                     $this.maxTemperature = [temperature]::new([convert]::ToDouble($weatherRequestsResults.main.temp_max))
-
-                    # Allocating the value of the choosen date and time format to the dateAndTimeFormatId class attribute
-                    $this.dateAndTimeFormatId = $choosenDateAndTimeFormat
 
                     ###########################################################################
                     # Extracting the UV index value and determine the UV risk...              #
---
+++ usedModules/dateandtime.psm1
@@ -0,0 +1,8 @@
+﻿# Definition of the dateAndTimeFormat Powershell enum to get the wished date and time format
+Enum dateAndTimeFormat {
+
+    TimestampFormat = 0
+    DMYHMSFormat = 1
+    YMDHMSFormat = 2
+    MDYHMSFormat = 3
+}
