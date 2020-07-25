dacb7f3e0bf111615e6815cf533e6b6bb03a1eb3
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-25 13:25:52 +0200

    Adding new class and others things

---
+++ usedModules/weatherDescription.psm1
@@ -0,0 +1,15 @@
+﻿# Definition of the weatherDescription Powershell class to manage weather datas
+class weatherDescription {
+
+    hidden [string]$mainDescription
+    hidden [string]$precisedDescription
+    hidden [string]$weatherURLIcon
+
+    # weatherDescription class constructor with all needed parameters
+    weatherDescription($mainDescription, $precisedDescription, $weatherIcon)
+    {
+        $this.mainDescription = $mainDescription
+        $this.precisedDescription = $precisedDescription
+        $this.weatherURLIcon = "https://openweathermap.org/img/wn/" + $weatherIcon + ".png"
+    }
+}
