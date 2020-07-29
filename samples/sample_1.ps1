d3e68163cb29d0cf771039b7cba9dfaf75e192e9
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-29 09:50:45 +0200

    Writting comments

--- samples/sample_1.ps1
+++ samples/sample_1.ps1
@@ -1,11 +1,11 @@
-﻿#
-Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell\UniversalWeatherPowershell.psm1
+﻿# Importation of the UniversalWeatherPowershell module
+Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell
 
-#
+# Definition of all parameters : '$city' for the wished city and '$apiKey' for the api key
 param (
     [string]$city,
     [string]$apiKey
 )
 
-# 
+# Definition of a new object of type 'universalWeatherPowershell' with '$city' and '$apiKey' as arguments
 $a = New-Object -TypeName universalWeatherPowershell -ArgumentList $city, $apiKey
