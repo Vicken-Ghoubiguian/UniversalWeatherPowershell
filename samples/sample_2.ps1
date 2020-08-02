2c8a7b01f6ef500f46162c0779a62bf2917549e1
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-02 15:27:14 +0200

    Adding 'ok' and 'cancel' buttons in the sample_2 form

--- samples/sample_2.ps1
+++ samples/sample_2.ps1
@@ -19,5 +19,23 @@ $form.Text = "Weather form"
 # Setting form size
 $form.Size = New-Object System.Drawing.Size(1000,1000)
 
+# Adding 'ok' button
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(800,800)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+# Adding 'cancel' button
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(900,800)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
 # Displaying form
 $form.ShowDialog() | Out-Null
