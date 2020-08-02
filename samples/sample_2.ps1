c988d4154263cd8250fceb7efaaeb397d43cd582
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-02 15:04:20 +0200

    Writting the sample 2

---
+++ samples/sample_2.ps1
@@ -0,0 +1,23 @@
+﻿# Importation of the UniversalWeatherPowershell module
+Using module C:\Users\ericg\Desktop\UniversalWeatherPowershell
+
+# 
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName PresentationFramework
+
+# Creation of the GUI
+$form = New-Object Windows.Forms.Form
+
+# Blocking the resizing of the form and remove the Minimize and Maximize icons
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+$form.MaximizeBox = $False
+$form.MinimizeBox = $False
+
+# Setting form title
+$form.Text = "Weather form"
+
+# Setting form size
+$form.Size = New-Object System.Drawing.Size(400,370)
+
+# Displaying form
+$form.ShowDialog() | Out-Null
