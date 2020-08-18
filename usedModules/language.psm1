763db3e96d442c456beb7a4f7dabf1d008c3db72
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-08-18 14:29:26 +0200

    Define setters for language class

--- usedModules/language.psm1
+++ usedModules/language.psm1
@@ -19,4 +19,56 @@ class language
         # Allocating the value of choosen language as member of the languageCode enum
         $this.choosenLanguage = [languageCode]::EN
     }
+
+    #
+    [void] setLanguageAsEnglish()
+    {
+        If($this.choosenLanguage -eq [languageCode]::EN)
+        {
+            # Message to display : "Choosen language already in english."
+        }
+        Else
+        {
+            $this.choosenLanguage = [languageCode]::EN
+        }
+    }
+
+    #
+    [void] setLanguageAsFrench()
+    {
+        If($this.choosenLanguage -eq [languageCode]::FR)
+        {
+            # Message to display : "Choosen language already in french."
+        }
+        Else
+        {
+            $this.choosenLanguage = [languageCode]::FR
+        }
+    }
+
+    #
+    [void] setLanguageAsGerman()
+    {
+        If($this.choosenLanguage -eq [languageCode]::DE)
+        {
+            # Message to display : "Choosen language already in german."
+        }
+        Else
+        {
+            $this.choosenLanguage = [languageCode]::DE
+        }
+    }
+
+    #
+    [void] setLanguageAsDanish()
+    {
+        If($this.choosenLanguage -eq [languageCode]::DA)
+        {
+            # Message to display : "Choosen language already in danish."
+        }
+        Else
+        {
+            $this.choosenLanguage = [languageCode]::DA
+        }
+    }
 }
